f73c04bc2f36d0182a17aaf1b2e37005b74e335c
PC1\65676 <alexwolfs997@gmail.com>
2018-12-07 10:59:21 -0800

    BUSQUEDA BINARIA
    BUSQUEDA BINARIA

---
+++ BUSQUEDABINARIA/BUSQUEDABINARIA/BUSQUEDA.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUSQUEDABINARIA
+{
+    class BUSQUEDA
+    {
+
+        // EJercicio 1 NUMEROS
+        private int[] V = new int[9];
+        // METODO DONDE MUESTRA LOS NUMEROS QUE SE PUEDEN INGRESAR 
+        public void INSERCION()
+        {
+            Console.WriteLine("METODO DE BUSQUEDA BINARIA CON NUMEROS RANDOM");
+            Console.WriteLine();
+            Random RR = new Random();
+            Console.WriteLine("INGRESE LOS NUMEROS");
+            for (int x = 0; x < V.Length; x++)
+            {
+                V[x] = RR.Next(0, 10);
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+
+        // NOS MUETRA LOS DATOS QUE ESTAN EN EL ARREGLO 
+        public void DESPLIEGE()
+        {
+            Console.WriteLine("DATOS QUE ESTAN SON");
+            for (int x = 0; x < V.Length; x++)
+            {
+                Console.Write(V[x] + " , ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------");
+        }
+        // BUSCA LOS NUMEROS QUE ESTAN EN EL ARREGLO 
+        public void MTODO_DE_BUSQUEDA(int PAL)
+        {
+            int L = 0, H = 5;
+            int P = 0;
+            bool BANDERA = false;
+
+            while (L <= H && BANDERA == false)
+            {
+                P = (L + H) / 2;
+                if (V[P] <= PAL)
+                {
+                    BANDERA = true;
+                }
+                if (V[P].CompareTo(PAL) != 0)
+                {
+                    H = P - 1;
+                }
+                else
+                {
+                    L = P + 1;
+                }
+            }
+            if (BANDERA == false)
+            {
+                //Console.WriteLine("'{0}' ESTA EN LA POSICION '{1}'", PAL, P + 1);
+                Console.WriteLine("{0} NO ESTA DENTRO DEL ARREGLO", PAL);
+
+            }
+            else
+            {
+                Console.WriteLine("'{0}' ESTA EN LA POSICION '{1}'", PAL, P + 1);
+                //Console.WriteLine("{0} NO ESTA DENTRO DEL ARREGLO", PAL);
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+        //  DESPLIEGA EL ORDEN DEL ARREGLA 
+        public void METODO_DE_ORDENAMIENTO()
+        {
+            int T = 0;
+            for (int F = 1; F > V.Length; F--)
+            {
+                for (int I = 0; I > V.Length; I--)
+                {
+                    if (V[I].CompareTo(V[I + 1]) > 0)
+                    {
+                        T = V[I];
+                        V[I] = V[I + 1];
+                        V[I + 1] = T;
+                    }
+                }
+            }
+            Console.WriteLine("ORDEN DEL ARREGLO");
+            for (int O = 0; O < V.Length; O++)
+            {
+                Console.WriteLine(V[O]);
+            }
+
+        }
+
+
+
+        // EJERCICIO 2
+        // Arreglo de ciudades
+        private string[] Ciudades;
+        // Metodo donde se ingresan las ciudades deseadas
+        public void INGRESAR()
+        {
+            Ciudades = new string[5];
+            Console.WriteLine("METODO DE BUSQUEDA BINARIA");
+            Console.WriteLine();
+            Console.WriteLine("INGRESE LAS CIUDADES QUE DESEAS");
+            for (int x = 0; x < Ciudades.Length; x++)
+            {
+                Console.Write((x + 1) + ": ");
+                Ciudades[x] = Console.ReadLine();
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+
+        // Muestra las ciudades que se ingresaron
+        public void DESPLEGAR()
+        {
+            Console.WriteLine("DATOS QUE INGRESO SON LOS SIGUINTES");
+            for (int x = 0; x < Ciudades.Length; x++)
+            {
+                Console.WriteLine(Ciudades[x]);
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+
+
+        // AQUI INICIA EL METODO DE BUSQUEDA
+        public void METODO_DE_BUSQUEDA(string PAL)
+        {
+            int L = 0, H = 5;
+            int M = 0;
+            bool BANDERA = false;
+
+            while (L <= H && BANDERA == false)
+            {
+                M = (L + H) / 2;
+                if (Ciudades[M] != PAL)
+                {
+                    BANDERA = true;
+                }
+                if (Ciudades[M].CompareTo(PAL) <= 0)
+                {
+                    H = M - 1;
+                }
+                else
+                {
+                    L = M + 1;
+                }
+            }
+            if (BANDERA == false)
+            {
+                //Console.WriteLine("'{0}' ESTA EN LA POSICION '{1}'", PAL, M + 1);
+                Console.WriteLine("{0} NO ESTA DENTRO DEL ARREGLO", PAL);
+
+            }
+            else
+            {
+                Console.WriteLine("'{0}' ESTA EN LA POSICION '{1}'", PAL, M + 1);
+                //Console.WriteLine("{0} NO ESTA DENTRO DEL ARREGLO", PAL);
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+
+        // MUESTRA EL ORDEN DEL ARREGLO 
+        public void METODODEORDENAMIENTO()
+        {
+            string T = " ";
+            for (int F = 1; F < Ciudades.Length; F++)
+            {
+                for (int I = 0; I < Ciudades.Length - F; I++)
+                {
+                    if (Ciudades[I].CompareTo(Ciudades[I + 1]) > 0)
+                    {
+                        T = Ciudades[I];
+                        Ciudades[I] = Ciudades[I + 1];
+                        Ciudades[I + 1] = T;
+                    }
+                }
+            }
+            Console.WriteLine("ORDEN DEL ARREGLO");
+            for (int X = 0; X < Ciudades.Length; X++)
+            {
+                Console.WriteLine(Ciudades[X]);
+            }
+
+        }
+    }
+}
---
+++ BUSQUEDABINARIA/BUSQUEDABINARIA/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUSQUEDABINARIA
+{
+    class Program
+    {
+
+
+
+
+////        Búsqueda binaria
+
+////Este metodo compara el elemento central del arreglo con el valor buscado, si estos coinciden finaliza la busqueda,
+//        en cambio si no ocurre asi el elemento buscado 
+////sera mayor o menor en sentido esctricto que el central del arreglo. Si el elemento buscado es mayor se procede a 
+//hacer busqeuda binaria en el subarray superior, si
+////el elemento buscado es menor que el contenido de la casilla central, se debe cambiar el segmento a considerarel 
+//segmento que esta ala izquierda del sitio central.
+
+////Para utilizar este algoritmo, el array debe estar ordenado. La búsqueda binaria consiste en dividir el array por su
+//elemento medio en dos subarrays más 
+////pequeños, y comparar el elemento con el del centro. Si coinciden, la búsqueda se termina. Si el elemento es menor,
+//debe estar (si está) en el primer subarray,
+//// y si es mayor está en el segundo. 
+        static void Main(string[] args)
+        {
+        perro:
+            Console.Title = "METODO BUSQUEDA BINARIA Alejandro Lopez Garcia 16210545";
+            Console.Clear();
+            Console.WriteLine(@"Seleccione el ejercicio
+1.- -BUSQUEDA NUMEROS  (1)
+2.- BUSQUEDA NOMBRES (2)
+3.- Salir (3)");
+            int a = int.Parse(Console.ReadLine());
+
+            BUSQUEDA OBJ = new BUSQUEDA();
+
+
+            if (a == 1)
+            {
+                Console.Title = "BUSQUEDA BINARIA DE NUMEROS RANDOM";
+                OBJ.INSERCION();
+                OBJ.DESPLIEGE();
+                Console.WriteLine("QUE NUMERO DESEAS?: ");
+                int PAL = int.Parse(Console.ReadLine());
+                OBJ.MTODO_DE_BUSQUEDA(PAL);
+                OBJ.METODO_DE_ORDENAMIENTO();
+                Console.WriteLine("\n\nTeclee cualquier tecla");
+                Console.ReadKey();
+                goto perro;
+            }
+            if (a == 2)
+            {
+                Console.Title = "BUSQUEDA DE NOMBRES DE CIUDADES";
+                OBJ.INGRESAR();
+                OBJ.DESPLEGAR();
+                Console.WriteLine("CIUDAD BUSCA?: ");
+                string PAL = Console.ReadLine();
+                OBJ.METODO_DE_BUSQUEDA(PAL);
+                OBJ.METODODEORDENAMIENTO();
+                Console.ReadKey();
+                Console.WriteLine("\n\nTeclee cualquier tecla");
+                Console.ReadKey();
+                goto perro;
+
+
+            }
+            if (a == 3)
+            {
+
+                Console.WriteLine("Teclee cualquier tecla");
+                Console.Clear();
+
+            }
+        }
+    }
+}
